f9288cb322ab26d0acbfdb5bc43a9f7a9cef8950
bevincass <peteraugros@gmail.com>
2020-02-10 14:34:10 -0700

    react files initial push

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      newItem: "",
+      list: []
+    }
+  }
+
+  updateInput(key, value){
+    //update react state
+    this.setState({
+      [key]: value
+    });
+  }
+
+  addItem() {
+    //create item with uniqu id
+    const newItem = {
+      id: 1 + Math.random(),
+      value: this.state.newItem.slice()
+    };
+
+    //copy of current list of items
+    const list = {...this.state.list};
+
+    //add new item to list
+    list.push(newItem);
+
+    //update state with new list and reset newItem input
+    this.setState({
+      list,
+      newItem:""
+    });
+  }
+
+
+  render() {
+    return (
+      <div className = "App">
+        <div>
+          Add an Item...
+          <br/>
+          <input 
+            type="text"
+            placehold="Type item here..."
+            value = { this.state.newItem }
+            onChange = {e => this.updateInput("newItem", e.target.value)}
+          />
+          <button
+            onClick={() => this.addItem()}
+          >
+            Add
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
